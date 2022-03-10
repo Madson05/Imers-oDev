8a07f7e35884c6dfb8caeec73246755a8f8607ba
Madson Silva <madsonsilva2005@gmail.com>
2022-03-10 20:25:51 -0300

    Adicionado desafio prático do segundo dia, um conversor de temperatura.

--- calculadoraDeMedia/assets/scripts/script.js
+++ calculadoraDeMedia/assets/scripts/script.js
@@ -1,14 +1,14 @@
 function calculaMedia(){
-    nota2 = parseFloat(document.getElementById("nota02").value);
-    nota3 = parseFloat(document.getElementById("nota03").value);
-    nota1 = parseFloat(document.getElementById("nota01").value);
-    nota4 = parseFloat(document.getElementById("nota04").value);
+    let nota2 = parseFloat(document.getElementById("nota02").value);
+    let nota3 = parseFloat(document.getElementById("nota03").value);
+    let nota1 = parseFloat(document.getElementById("nota01").value);
+    let nota4 = parseFloat(document.getElementById("nota04").value);
 
-    media = ((nota1 + nota2 + nota3 + nota4) / 4).toFixed(1);
+    let media = ((nota1 + nota2 + nota3 + nota4) / 4).toFixed(1);
 
-    situacao = media >= 6 ? "Aprovado" : "Reprovado";
+    let situacao = media >= 6 ? "Aprovado" : "Reprovado";
 
-    resultado = document.getElementById("result");
+    let resultado = document.getElementById("result");
 
     resultado.innerHTML = "Situação: " + situacao + ". Sua Media final foi: " + media;
 
