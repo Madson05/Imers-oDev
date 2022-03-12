65f3377cac29d42fd9ef9889d8e893861caa22e9
Madson Silva <madsonsilva2005@gmail.com>
2022-03-12 16:30:55 -0300

    Atualizei o código do programa usando funções e também foi adicionado mais uma veirficação dos dados inseridos enquadrando-se assim no que foi solicitado  na aula 05 da imersão DEV.

--- FavFlix/assets/javaScript/script.js
+++ FavFlix/assets/javaScript/script.js
@@ -1,54 +1,61 @@
 let listaSeries = [];
 
-function inserirSerie(){
+function clearCampos(){
+    document.getElementById("tituloSerie").value = "";
+    document.getElementById("linkSerie").value = "";
+}
+
+function validaDados(){
+
     let titulo = document.getElementById("tituloSerie").value;   
-    let link = document.getElementById("linkSerie").value;
-    let conteudo = document.getElementById("series");
+    let url = document.getElementById("linkSerie").value;
 
-    let dadosSerie = [];
+    let erroEncontrado = false;
 
-        let verificacao = false;
+    if(!url.endsWith("jpg") || titulo == ""){
+        alert("URL informada é inválida ou o campo titulo está vazio, tente novamente!");
+        clearCampos();
+        erroEncontrado = true;
+    }
 
-    
-        if(listaSeries.length !== 0){
-            for(serie of listaSeries){
-                if(serie[0] === titulo || serie[1] === link){
-                    alert("Este titulo já foi adicionado, tente outro. ");
-                    verificacao = false;
-                    break;
-                }else{
-                    verificacao = true;
-                }
-
-                
+    else if(listaSeries.length > 0){
+        for(serie of listaSeries){
+            if(serie[0] === titulo || serie[1] === url){
+                alert("Este titulo já foi adicionado.")
+                erroEncontrado = true;
+                clearCampos();
+                break;
             }
-
         }
-        
-        if(listaSeries.length === 0 || verificacao === true){
-            dadosSerie.push(titulo);
-            dadosSerie.push(link);
-            listaSeries.push(dadosSerie);
-            dadosSerie = [];
-        
-            console.log(listaSeries)
-            conteudo.innerHTML = ""
-            for(let i = 0; i < listaSeries.length; i++){
-                     
-                conteudo.innerHTML += `<div class = "serie"> <img class = "imagemSerie" src = "${listaSeries[i][1]} "> <br> <h3 class = "tituloSerie">${listaSeries[i][0]}</h3> </div>`;
-        
-                
-        
-            }
-        
-        
-
+    }
+    if(erroEncontrado === false){
+        clearCampos();
+        cadastrarSerie(titulo, url);
     }
 
 
+}
 
 
+function cadastrarSerie(titulo, url){
 
+    let dadosSerie = [];
 
+    dadosSerie.push(titulo);
+    dadosSerie.push(url);
+    
+    listaSeries.push(dadosSerie);
+    
+    visualizarDados()
+}
+
+function visualizarDados(){
+conteudo = document.getElementById("series")
+
+    conteudo.innerHTML = ""
+            for(let i = 0; i < listaSeries.length; i++){
+                     
+                conteudo.innerHTML += `<div class = "serie"> <img class = "imagemSerie" src = "${listaSeries[i][1]} " alt = "imagem de capa da serie ${listaSeries[i][0]}"> <br> <h3 class = "tituloSerie">${listaSeries[i][0]}</h3> </div>`;
+            }
 
 }
