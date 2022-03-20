5a34fb1427017048509b31ffe05a1eac4cad8039
Madson Silva <madsonsilva2005@gmail.com>
2022-03-19 23:02:07 -0300

    criada nova versão do joguingo, com umvisual mais legal do que o anterior.

--- superTrunfo/assets/javaScript/script.js
+++ superTrunfo/assets/javaScript/script.js
@@ -1,6 +1,7 @@
 let cartas = [
     carta1 = {
-        nome: "Harvey",
+        nome: "Harvey Specter",
+        imagem: "https://i.pinimg.com/originals/09/b1/5a/09b15a8627b93106f125325b112bdb26.jpg",
         atributos: {
             Acusação: 10,
             Defesa: 8,
@@ -8,7 +9,8 @@ let cartas = [
         }    
     },
     carta2 = {
-        nome: "Jessica",
+        nome: "Jessica Pearson",
+        imagem: "https://pbs.twimg.com/profile_images/1039666848852320256/iDZ6394V_400x400.jpg",
         atributos:{
             Acusação: 8,
             Defesa: 10,
@@ -16,7 +18,8 @@ let cartas = [
         }  
     },
     carta3 = {
-        nome: "Mike",
+        nome: "Mike Ross",
+        imagem: "https://i.pinimg.com/originals/1e/83/cf/1e83cf51c8012a56c3dc145dbd611c5f.jpg",
         atributos:{
             Acusação: 8,
             Defesa: 10,
@@ -36,21 +39,47 @@ function sortearCartas(){
         indiceJogador = parseInt((Math.random() * 3));
     }
     cartaJogador = cartas[indiceJogador];
-    exibirOpcoes()
+    exibirImagemJogador()
     document.getElementById("sortCartas").disabled = true;
     document.getElementById("buttonPlay").disabled = false;
 }
 
-function exibirOpcoes() {
-    opcoes = document.getElementById("opcoes");
+function exibirImagemJogador() {
+    let tituloCarta = document.getElementById("titulo-carta-jogador")
+    let imagem = document.getElementById("imagem-carta-jogador")
+
+    tituloCarta.innerHTML=`${cartaJogador.nome}`
+    imagem.style.backgroundImage += `url(${cartaJogador.imagem})`;
+    imagem.style.backgroundSize = "100%"
+    exibirOpcoes();
+}
+
+function exibirImagemMaquina() {
+    let atributosMaquina = document.getElementById("atributos-carta-maquina")
+    let tituloCarta = document.getElementById("titulo-carta-maquina")
+    let imagem = document.getElementById("imagem-carta-maquina")
+
+    tituloCarta.innerHTML=`${cartaMaquina.nome}`
+    imagem.style.backgroundImage += `url(${cartaMaquina.imagem})`;
+    imagem.style.backgroundSize = "100%"
+    for(item in cartaMaquina.atributos){
+        console.log(cartaMaquina.atributos.item)
+        atributosMaquina.innerHTML += `<label>${item} (${cartaMaquina.atributos[item]})</label><br>`
+    }
+}
+    
+function exibirOpcoes(){
+    let opcoes = document.getElementById("atributos-carta-jogador");
     let opcoesOfertadas = "";
     for(opcao in cartaJogador.atributos){
-
+        
         opcoesOfertadas += `<input type = "radio" name = "atributo" id = "${opcao}" value = "${opcao}">
-        <label for = "${opcao}">${opcao}<label/>`
+        <label for = "${opcao}">${opcao} (${cartaJogador.atributos[opcao]})<label/><br>`
     }
     opcoes.innerHTML = opcoesOfertadas;
 }
+
+
 
 function obtemOpcaoSelecionada(){
     let opcoes = document.getElementsByName("atributo")
@@ -69,20 +98,16 @@ function jogar(){
     let valorSelecionadoMaquina = cartaMaquina.atributos[elementoSelecionado];
 
     if(valorSelecionadoMaquina < valorSelecionadoJogador){
-        resultado.innerHTML = "Você ganhou! "
+        resultado.innerHTML = "<h2>Você ganhou!</h2>"
 
     }else if(valorSelecionadoMaquina>valorSelecionadoJogador){
-        resultado.innerHTML = "Você perdeu!"
+        resultado.innerHTML = "<h2>Você perdeu!</h2>"
     }else{
-        resultado.innerHTML = "Você empatou"
+        resultado.innerHTML = "<h2>Você empatou</h2>"
     }
-
-    resultado.innerHTML += `<br>
-    <h3>Sua carta era: ${cartaJogador.nome}<br>
-    O seu poder na opção escolhida foi: ${valorSelecionadoJogador}<br><br>
-    A carta da Maquina era: ${cartaMaquina.nome}<br>
-    E o valor da máquina na opção selecionada foi: ${valorSelecionadoMaquina}
-    </h3>`
+    resultado.style.backgroundColor = "black"
+    resultado.style.width = "100%"
+    exibirImagemMaquina();
 
 
 }
