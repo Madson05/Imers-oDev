72319c7691b83e6f93ccd6b3f602132bad4f227a
Madson Silva <madsonsilva2005@gmail.com>
2022-03-20 21:43:19 -0300

    adicionado link de vizualização através do GitHub Pages, adicionado modo escuro/dark mode e a estrutura de pasta foi melhorada.

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,34 +1,46 @@
-let areaProjetos = document.getElementById("item-projeto")
+function alterarTema(){
+    document.body.classList.toggle("dark")
+}
 
+
+
+
+let areaProjetos = document.getElementById("item-projeto")
 let projetos = [
     projeto1  = {
         nome: "Calculadora de Média",
         icone: "📝",
+        linkView: "https://madson05.github.io/ImersaoDev/projetos/calculadoraDeMedia/",
         link: "https://github.com/Madson05/ImersaoDev/tree/main/calculadoraDeMedia",
     },
     projeto2  = {
         nome: "Conversor de Temperatura",
         icone: "🌡",
+        linkView: "https://madson05.github.io/ImersaoDev/projetos/conversorDeTemperatura/",
         link: "https://github.com/Madson05/ImersaoDev/tree/main/conversorDeTemperatura",
     },
     projet3  = {
         nome: "Mentalista",
         icone: "❓",
+        linkView: "https://madson05.github.io/ImersaoDev/projetos/Mentalista/",
         link: "https://github.com/Madson05/ImersaoDev/tree/main/Mentalista",
     },
     projeto4  = {
         nome: "FavFlix",
         icone: "🎥",
+        linkView: "https://madson05.github.io/ImersaoDev/projetos/FavFlix/",
         link: "https://github.com/Madson05/ImersaoDev/tree/main/FavFlix",
     },   
     projeto5  = {
         nome: "Tabela de Classificação",
         icone: "🏅",
+        linkView: "https://madson05.github.io/ImersaoDev/projetos/tabelaDeClassificacao/",
         link: "https://github.com/Madson05/ImersaoDev/tree/main/tabelaDeClassificacao",
     },
     projeto6  = {
         nome: "SuperTrunfo",
         icone: "🃏",
+        linkView: "https://madson05.github.io/ImersaoDev/projetos/superTrunfo/index.html",
         link: "https://github.com/Madson05/ImersaoDev/tree/main/superTrunfo",
     }
 ];
@@ -38,14 +50,19 @@ for(let indice = 0; indice < projetos.length; indice ++){
     
     areaProjetos.innerHTML += `
     <li id = "projeto${indice}" class = "items">
-        <a href = "${projetos[indice].link}">${projetos[indice].nome}</a>
+        <a href = "${projetos[indice].linkView}">${projetos[indice].nome} - Site 🌐</a>
     </li>
+
+    <a href = "${projetos[indice].link}" class = "texto-codigo">Clique aqui para ver o código👨🏾‍💻</a>
+
+
     
     <style>
         #projeto${indice}::before{
             content: '${projetos[indice].icone}';
         }
         #projeto${indice}{
+            margin-top: 30px;
             margin-bottom: 5px;
         }
     </style>`
